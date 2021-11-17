6c243dc920bb91e4932a948ab4c0621b498e7668
Dinopandab <coronalizbeth567@gmail.com>
2021-11-17 12:42:20 -0500

    notes lesson 1

--- L2-If Else Statements/src/ShippingCosts.java
+++ L2-If Else Statements/src/ShippingCosts.java
@@ -1,13 +1,15 @@
+import javax.swing.*;
+
 public class ShippingCosts {
 
     /*
     A Shipping Company charges the following rates
     for each 100 miles shipped:
 
-    2lbs or less ..................... $1.10
-    over 2lbs but less than 6lbs ..... $2.20
-    over 6lbs but less than 10lbs .... $3.70
-    over 10lbs ....................... $4.80
+    2 lbs or less ..................... $1.10
+    over 2 lbs but less than 6 lbs ..... $2.20
+    over 6 lbs but less than 10 lbs .... $3.70
+    over 10 lbs ....................... $4.80
 
     Write a program to calculate the shipping price. The
     distance is not prorated, in other words, the price doesn't
@@ -22,5 +24,42 @@ public class ShippingCosts {
     As always, your program should include a method.
      */
 
+
+    public static void main(String[] args) {
+
+        double itemWeight = Double.parseDouble(JOptionPane.showInputDialog("How much is the weight? "));
+        int milesShipped = Integer.parseInt(JOptionPane.showInputDialog("how many miles will it ship? "));
+        double shippingRate;
+        double cost;
+
+        if(itemWeight >= 10){
+            shippingRate = 4.80;
+        }else{
+            if (itemWeight >= 6){
+                shippingRate = 3.70;
+            }else{
+                if (itemWeight >= 2){
+                    shippingRate = 2.20;
+                }else{
+                    shippingRate = 1.10;
+                }
+            }
+        }
+    }
+
+
+    }
+
+    public static double cost(double rate, int miles) {
+
+        int costMultiplier = miles/100;
+
+        if(costMultiplier == 0){
+            costMultiplier = 1;
+        }
+
+        return rate*costMultiplier;
+
+    }
 
 }
--- L2-If Else Statements/src/Validation.java
+++ L2-If Else Statements/src/Validation.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Validation {
 
     /*
@@ -6,7 +8,16 @@ public class Validation {
 
     Let's say a user must enter a number greater than 10.
      */
+    public static void main(String[] args) {
 
+         int number = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number greater them 10"));
+
+         if(number > 10){
+             JOptionPane.showMessageDialog(null, "you entered a correct number.");
+         }else {
+             JOptionPane.showMessageDialog(null, "invalid input. program closing.");
+         }
+    }
 
 
 }
